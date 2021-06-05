5cbcb84f0d7d9a276b472dc229518f47fdf01582
Sma-source <rabhi.smain@gmail.com>
2021-06-05 18:10:26 +0200

    modify Note: add ui

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { MdDeleteForever } from "react-icons/md";
 
 const Note = () => {
-  return <div className="note"></div>;
+  return (
+    <div className="note">
+      <span>First nite Hello</span>
+      <div className="note-footer">
+        <small>13/06/21</small>
+        <MdDeleteForever className="delete-icon" size="1.3em" />
+      </div>
+    </div>
+  );
 };
 
 export default Note;
