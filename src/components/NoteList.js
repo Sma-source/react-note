fa677a41ab60ba832b47ff05e4a40665f9ad04bb
Sma-source <rabhi.smain@gmail.com>
2021-06-05 18:12:10 +0200

    import Note

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Note from "./Note";
 
 const NoteList = () => {
-  return <div className="notes-list"></div>;
+  return (
+    <div className="notes-list">
+      <Note />
+    </div>
+  );
 };
 
 export default NoteList;
