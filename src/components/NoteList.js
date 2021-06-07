505d18e73d0aa7c6f55ba2da10de4e7a264182f1
Sma-source <rabhi.smain@gmail.com>
2021-06-07 15:04:45 +0200

    map to notes

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -4,7 +4,9 @@ import Note from "./Note";
 const NoteList = ({ notes }) => {
   return (
     <div className="notes-list">
-      <Note />
+      {notes.map((note) => (
+        <Note id={note.id} text={note.text} date={note.date} />
+      ))}
     </div>
   );
 };
