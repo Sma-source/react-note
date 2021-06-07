246c935affce7fcb190d5f7421f21328e400fc5a
Sma-source <rabhi.smain@gmail.com>
2021-06-07 16:04:47 +0200

    update AddNote: handle textarea change and button click to add new Note

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AddNote = () => {
+const AddNote = ({ handleAddNote }) => {
+  const [notetext, setNoteText] = useState("");
+
+  const handleChange = (e) => {
+    setNoteText(e.target.value);
+  };
+  const handleSaveClick = () => {
+    if (notetext.trim().length > 0) {
+      handleAddNote(notetext);
+      setNoteText("");
+    } else {
+      setNoteText("");
+    }
+  };
   return (
     <div className="note new">
       <textarea
         cols="10"
         rows="8"
         placeholder="Type to add a note..."
+        value={notetext}
+        onChange={handleChange}
       ></textarea>
       <div className="note-footer">
         <small>200 remainings</small>
-        <button className="save">Save</button>
+        <button className="save" onClick={handleSaveClick}>
+          Save
+        </button>
       </div>
     </div>
   );
