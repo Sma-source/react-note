1dc701cb8b1fcaaeb28393f17de534f9a1162f62
Sma-source <rabhi.smain@gmail.com>
2021-06-07 15:13:03 +0200

    add id as key

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -5,7 +5,7 @@ const NoteList = ({ notes }) => {
   return (
     <div className="notes-list">
       {notes.map((note) => (
-        <Note id={note.id} text={note.text} date={note.date} />
+        <Note key={note.id} text={note.text} date={note.date} />
       ))}
     </div>
   );
