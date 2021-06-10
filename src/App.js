07287bddf6d354def71ba71a39a313bfacdee568
Sma-source <rabhi.smain@gmail.com>
2021-06-10 19:13:05 +0200

    saved new note to local storage

--- src/App.js
+++ src/App.js
@@ -33,6 +33,14 @@ function App() {
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
+
+    if (savedNotes) {
+      setNotes(savedNotes);
+    }
+  }, []);
+
+  useEffect(() => {
     localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
   }, [notes]);
 
