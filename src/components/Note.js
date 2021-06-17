41baaee0d6da5736322fd8fad429f732fc334401
Sma-source <rabhi.smain@gmail.com>
2021-06-17 17:56:49 +0200

    import Popup ,create const to trigger btn popup

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdDeleteForever } from "react-icons/md";
+import Popup from "./Popup";
 
 const Note = ({ id, text, date, handleDeleteNote }) => {
+  const [btnPop, setBtnPop] = useState(false);
   return (
     <div className="note">
       <span>{text}</span>
       <div className="note-footer">
         <small>{date} </small>
-        <MdDeleteForever
+
+        {/* <MdDeleteForever
           onClick={() => handleDeleteNote(id)}
           className="delete-icon"
           size="1.3em"
+        /> */}
+        <MdDeleteForever
+          onClick={() => setBtnPop(true)}
+          className="delete-icon"
+          size="1.3em"
         />
+        <Popup
+          trigger={btnPop}
+          setTrigger={setBtnPop}
+          handleDeleteNote={handleDeleteNote}
+          id={id}
+        >
+          <h3>Voulez vous vraiment supprimer cette note ?</h3>
+        </Popup>
       </div>
     </div>
   );
