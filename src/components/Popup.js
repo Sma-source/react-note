2dd9e69cf67e26f6308cb881afba9e7c3222c4eb
Sma-source <rabhi.smain@gmail.com>
2021-06-17 17:57:48 +0200

    add ui, and props drilling to delete not or close popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Popup = () => {
-  return <div></div>;
+const Popup = ({ trigger, setTrigger, children, id, handleDeleteNote }) => {
+  return trigger ? (
+    <div className="popup">
+      <div className="popup-inner">
+        {children}
+        <div className="btn-contain">
+          <button className="close-btn" onClick={() => handleDeleteNote(id)}>
+            Oui
+          </button>
+          <button className="close-btn" onClick={() => setTrigger(false)}>
+            Non
+          </button>
+        </div>
+      </div>
+    </div>
+  ) : (
+    ""
+  );
 };
 
 export default Popup;
