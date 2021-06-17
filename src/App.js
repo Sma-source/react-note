c98c73f9d49f69c3bd6de4bf516ec9a48240dbaa
Sma-source <rabhi.smain@gmail.com>
2021-06-17 23:14:24 +0200

    delete note

--- src/App.js
+++ src/App.js
@@ -5,28 +5,7 @@ import Search from "./components/Search";
 import Header from "./components/Header";
 
 function App() {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      text: "Une note comme ça",
-      date: "10/06/21",
-    },
-    {
-      id: nanoid(),
-      text: "Si tu n'existais pas ...",
-      date: "12/06/21",
-    },
-    {
-      id: nanoid(),
-      text: "Encore une fois!",
-      date: "13/06/21",
-    },
-    {
-      id: nanoid(),
-      text: "Ceci est inacceptable",
-      date: "14/06/21",
-    },
-  ]);
+  const [notes, setNotes] = useState([]);
 
   const [searchText, setSearchText] = useState("");
 
